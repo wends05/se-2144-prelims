3a0efc20e7994f30510a3b6a54fef620ee746417
wends05 <cxnner05@gmail.com>
2024-10-01 23:17:39 +0800

    ang decimal sa next operator

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,66 +1,82 @@
 import { hellos } from "./displayHello"
 import { equationDisplay, config, resultDisplay, calculatorDisplay } from "./main"
 
-const MAX_CHARACTERS = 10
+const MAX_CHARACTERS = 12
 let canAddDecimal = true
 let current_number = ""
 
+
 export const inputCharacter = (value: string) => {
 
-  let tempDisplay = resultDisplay!.innerHTML;
-
   if (!config.isOn) return;
+
+  let tempDisplay = resultDisplay!.innerHTML;
+  const operators = ["-", "+", "x", "/", "*"]
 
   if (hellos.includes(tempDisplay) || value == "AC" || value == "Delete") {
     tempDisplay = "0";
     equationDisplay!.innerHTML = ""
+    current_number = ""
+    canAddDecimal = true
   }
 
   if (value == "C" || value == "Backspace") {
+
+    if (tempDisplay.endsWith(".")) canAddDecimal = true;
+
+    // check if to be backspaced is an operator
+    if (operators.some(op => tempDisplay.endsWith(op))) {
+      // split per operator to string[]
+      const splitNumbers = tempDisplay.split(/[+\-*/]/)
+      // false canAddDecimal if previous number has a decimal
+      // last index of string [] is empty string
+      if (splitNumbers[splitNumbers.length - 2].includes(".")) {
+        console.log("last number on the display includes a .")
+        canAddDecimal = false
+      }
+    }
+
+    if (tempDisplay == "0" || !tempDisplay.includes(".")) {
+      canAddDecimal = true
+    }
+
     tempDisplay = tempDisplay.slice(0, resultDisplay!.innerHTML.length - 1)
     if (tempDisplay.length == 0) {
       tempDisplay = "0"
     }
   }
   
-  const operators = ["-", "+", "x", "/", ".", "*"]
   
   if (Number.parseInt(value) ||
+      value == "." ||
       operators.includes(value) ||
       (value == "0" && resultDisplay!.innerHTML != "")) {
+    
+    if (resultDisplay!.innerHTML.length > MAX_CHARACTERS) return;
         
     if (tempDisplay.startsWith("0") && tempDisplay.length == 1 && !operators.includes(value)) {
       tempDisplay = ""
     }
-    console.log(Number.parseInt(value))
 
-    if (!tempDisplay.includes(".") || value != ".") {
+    
+    if (canAddDecimal || value != ".") {
       tempDisplay += value
       tempDisplay = tempDisplay.replace("x", "*")
     }
   }
 
-  if (value == "=" || value == "Enter") {
+  if (["=", "Enter"].includes(value)) {
     equationDisplay!.innerHTML = tempDisplay
     const x = tempDisplay
     try {
-      console.log(x)
       const res = eval(x)
 
       if (res == Infinity || isNaN(res)) {
         throw Error
       }
-      console.log(res);
       tempDisplay = res.toString()
-      if (tempDisplay.split(".").length == 2) {
-        console.log(tempDisplay.split("."))
-
-        const fracDigits = tempDisplay.split(".")[1].length
-        console.log(tempDisplay)
-        
-        tempDisplay = Number.parseFloat(tempDisplay).toFixed(fracDigits-1)
-        console.log(tempDisplay)
-      }
+      // has a decimal result
+      
     } catch (e) {
       console.log(e)
       resultDisplay!.innerHTML = "Math Error"
@@ -70,24 +86,38 @@ export const inputCharacter = (value: string) => {
 
   let final_display = tempDisplay
 
-  if (final_display.length > MAX_CHARACTERS) {
-    final_display = final_display.slice(0, MAX_CHARACTERS)
-    console.log(`current display (max ${MAX_CHARACTERS} characters), `, final_display)
+  if (operators.includes(value)) {
+    canAddDecimal = true
+    current_number = ""
   }
+  if (Number.parseInt(value)) {
+    current_number += value
+  }
+  if (value == "." && canAddDecimal) {
+    canAddDecimal = false
+  }
+  
+  console.log(current_number)
+  console.log(canAddDecimal)
+
+  // removes "." if its the last character
   if (final_display.charAt(MAX_CHARACTERS) == ".") {
     final_display = final_display.slice(0, MAX_CHARACTERS -1)
     console.log("current display (with '.'), ", final_display)
   }
+
+  // set max characters
+  if (final_display.length > MAX_CHARACTERS) {
+    final_display = final_display.slice(0, MAX_CHARACTERS)
+    console.log(`current display (max ${MAX_CHARACTERS} characters), `, final_display)
+  }
+  // round off result so that number with decimal is still up to Max Characters
+  if (final_display.split(".").length == 2 && ["=", "Enter"].includes(value)) {
+    const fracDigits = final_display.split(".")[1].length
+    final_display = Number.parseFloat(tempDisplay).toFixed(fracDigits)
+  }
   console.log(final_display)
   console.log(tempDisplay)
-
-  if (final_display.includes(".")) {
-    canAddDecimal = false
-  }
-  if (operators.includes("value")) {
-    canAddDecimal = true
-    current_number = ""
-  }
 
   resultDisplay!.innerHTML = final_display
 }
